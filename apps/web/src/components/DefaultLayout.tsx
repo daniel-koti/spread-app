226b5c9045e6419740e5c7f5315e3952666d3427
Daniel Moniz <dkmoniz@cetim.ms>
2023-05-09 10:27:28 +0100

    feat: Layout finished

--- apps/web/src/components/DefaultLayout.tsx
+++ apps/web/src/components/DefaultLayout.tsx
@@ -8,9 +8,9 @@ interface DefaultLayoutProps {
 
 export function DefaultLayout({ children }: DefaultLayoutProps) {
   return (
-    <div className="flex gap-2 h-screen bg-zinc-800 p-4">
+    <div className="flex gap-2 h-screen bg-zinc-800 p-4 overflow-hidden">
       <Sidebar />
-      <main className="flex-1 bg-white p-4 rounded-3xl overflow-hidden">
+      <main className="flex-1 bg-white p-4 rounded-3xl">
         <Header />
         <div className="overflow-auto h-full mt-4 py-6">{children}</div>
       </main>
